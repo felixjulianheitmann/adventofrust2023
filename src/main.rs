47edc7b7fd7bab7630fe695b98c455f3beb11477
Felix <felixjulianheitmann@gmail.com>
2023-12-03 14:22:24 +0100

    solution 3 - 2

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,10 @@ fn main() {
             let subline = &line[idx_x..idx_x + dot_pos];
             let num = match subline.parse::<i32>() {
                 Ok(v) => v,
-                Err(_) => {println!("couldn't parse: {}", subline); continue},
+                Err(_) => {
+                    println!("couldn't parse: {}", subline);
+                    continue;
+                }
             };
 
             // collect number + coordinates
@@ -53,36 +56,69 @@ fn main() {
     let mut sum = 0;
     let h = lines.len() as i32;
     let w = lines[0].len() as i32;
-    // check for sorrounding symbols
-    'number_loop: for number in numbers {
-        for (x, y) in number.coords {
+
+    // Check all stars for numeric neihbors
+    for (y, line) in lines.iter().enumerate() {
+        for (x, c) in line.chars().enumerate() {
             let x = x as i32;
             let y = y as i32;
-
-            let check = |sum: &mut i32, x: i32, y: i32, h, w| -> bool {
+            let check = |h, w, x: i32, y: i32| -> bool {
                 if x >= 0 && x < w && y >= 0 && y < h {
                     let line = &lines[y as usize];
                     let c = line.chars().nth(x as usize).unwrap();
-                    if !c.is_numeric() && c != '.' {
-                        *sum += number.value;
+                    if c.is_numeric() {
                         return true;
                     }
                 }
                 false
             };
+            if c == '*' {
+                let ul = check(h, w, x - 1, y - 1);
+                let ur = check(h, w, x + 1, y - 1);
+                let uc = check(h, w, x, y - 1);
+                let bl = check(h, w, x - 1, y + 1);
+                let br = check(h, w, x + 1, y + 1);
+                let bc = check(h, w, x, y + 1);
+                let cl = check(h, w, x - 1, y);
+                let cb = check(h, w, x + 1, y);
+                let mut n_numeric_neighbors = ul as i32
+                    + ur as i32
+                    + uc as i32
+                    + bl as i32
+                    + br as i32
+                    + bc as i32
+                    + cl as i32
+                    + cb as i32;
+                if bl && bc{
+                    n_numeric_neighbors -= 1
+                }
+                if bc && br{
+                    n_numeric_neighbors -= 1
+                }
+                if ul && uc{
+                    n_numeric_neighbors -= 1
+                }
+                if uc && ur{
+                    n_numeric_neighbors -= 1
+                }
+                if n_numeric_neighbors == 2 {
+                    let mut neighbors = numbers.iter().filter(|n| {
+                        n.coords.iter().any(|coord| {
+                            coord == &((x - 1) as usize, (y - 1) as usize)
+                                || coord == &((x + 1) as usize, (y - 1) as usize)
+                                || coord == &(x as usize, (y - 1) as usize)
+                                || coord == &((x - 1) as usize, (y + 1) as usize)
+                                || coord == &((x + 1) as usize, (y + 1) as usize)
+                                || coord == &(x as usize, (y + 1) as usize)
+                                || coord == &((x - 1) as usize, y as usize)
+                                || coord == &((x + 1) as usize, y as usize)
+                        })
+                    });
 
-            // check all neighbors for symbols
-            if check(&mut sum, x - 1, y - 1, h, w)
-                || check(&mut sum, x + 1, y - 1, h, w)
-                || check(&mut sum, x, y - 1, h, w)
-                || check(&mut sum, x - 1, y + 1, h, w)
-                || check(&mut sum, x + 1, y + 1, h, w)
-                || check(&mut sum, x, y + 1, h, w)
-                || check(&mut sum, x - 1, y, h, w)
-                || check(&mut sum, x + 1, y, h, w)
-            {
-                // println!("Number {} has an adjacent symbol", number.value);
-                continue 'number_loop;
+                    let gear_ratio =
+                        neighbors.next().unwrap().value * neighbors.next().unwrap().value;
+                    sum += gear_ratio;
+                }
             }
         }
     }
